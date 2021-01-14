a574fa5d0dbb6c997db18682af2433bb2a0f05a8
zaqqson <74024722+zaqqson@users.noreply.github.com>
2021-01-15 00:47:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def main():
 
     length = 0
     azimuth = 0
+    angle= 0
     for i in range(len(points)-1):
         azimuth += points[i].get_azimuth(points[i+1])
         length += points[i].get_length(points[i+1])
+        angle += points [i].get_angle(points[], points[])
 
     print(f'length = {length}')
     print(f'azimuth = {azimuth}')
+    print(f'angle = {angle}')
 
 
 if __name__ == '__main__':
